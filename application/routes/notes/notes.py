e2a7705c54db857bdb012f23c251d76c37dc565a
Karol Magier <karol.magier@gmail.com>
2020-07-01 15:17:10 +0200

    Added file download/upload

--- application/routes/notes/notes.py
+++ application/routes/notes/notes.py
@@ -1,5 +1,6 @@
-from flask import Flask, Blueprint, current_app, request, url_for, redirect, render_template, jsonify
+from flask import Flask, Blueprint, current_app, request, url_for, redirect, render_template, jsonify, send_file
 from database import db
+import random, string
 from forms.forms import NoteForm
 from models.user import User
 from flask_login import login_required, current_user
@@ -8,6 +9,7 @@ from models.note import Note
 
 
 notes_bp = Blueprint('notes', __name__)
+DIR_PATH = 'static/files/'
 
 @notes_bp.route('/add-note', methods=["GET", "POST"])
 @login_required
@@ -17,13 +19,37 @@ def add_note():
     if request.method == "POST" and form.validate_on_submit():
         title = form.title.data
         description = form.description.data
-        note = Note(title=title, description=description, owner_id=user.id)
-        db.session.add(note)
+        attachment = request.files[form.attachment.name]
+        if attachment:
+            filename_prefix = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
+            new_filename = filename_prefix + '.' + attachment.filename.split('.')[-1]
+            path_to_file = DIR_PATH + new_filename
+            attachment.save(path_to_file)
+            note = Note(title=title, description=description, file_path=path_to_file, 
+                        org_attachment_filename=attachment.filename, attachment_hash = new_filename, owner_id=user.id)
+            current_app.logger.debug(f'Note with file: {attachment.filename}')
+            db.session.add(note)
+        else:
+            note = Note(title=title, description=description, owner_id=user.id)
+            current_app.logger.debug(f'No file')
+            db.session.add(note)
         db.session.commit()
         current_app.logger.debug("Note added")
         current_app.logger.debug(f"Content: {description}")
         return redirect(request.url)
     return render_template("add-note.html", form=form)
+
+@notes_bp.route('/private-notes/<string:file_hash>', methods=["GET", "POST"])
+@login_required
+def download_file(file_hash):
+    file_to_download = Note.query.filter_by(attachment_hash=file_hash).first()
+    if file_to_download:
+        path_to_file = file_to_download.file_path
+        org_filename = file_to_download.org_attachment_filename
+        try:
+            return send_file(path_to_file, attachment_filename = org_filename, as_attachment = True)
+        except Exception as e:
+            print(e, file = sys.stderr)
 
 @notes_bp.route('/private-notes', methods=["GET", "POST"])
 @login_required
